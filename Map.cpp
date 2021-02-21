04bf234c72d17bcbb2280ba8777d6be7ed66969e
CodeLover82 <72126291+CodeLover82@users.noreply.github.com>
2021-02-21 21:01:35 +0900

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -1,20 +1,46 @@
-- 해시(map) --------------------------------------------------------
+[ Map ]
+각 노드가 key와 value 쌍으로 이루어진 트리이다. 
 
-기본적으로 key값의 오름차순으로 정렬된다.
-정렬방법을 바꾸려면, 직접 구조체를 정의해 연산자 오버로딩을 해야 함.
+< 특징 >
+key의 중복을 허용하지 않는다.
+내부 구현은 검색, 삽입, 삭제가 O(logn)인 레드블랙트리로 구성.
+자료를 저장할 때 자동으로 key를 기준으로 오름차순 정렬한다.
 
-include <map>
-map<string, int> mp;  // map<key, value> int 형의 value는 0으로 초기화
-원소 추가 : mp["a"]++;
-원소 삭제 : mp.erase("a");
-value 값 : mp.count("a");
-순회 : 
-for(map<string, int>::iterator iter = mp.begin();iter != mp.end();iter++) {
+	
+< 선언 >
+map<key, value> m;
+ex) map<string, int> m;
+
+< 원소 추가 >
+map.insert(key, value);  // key가 중복된다면 insert가 수행되지 않는다.
+ex) m.insert("x", 3);
+
+< value 값 변경 >
+map[key] = value;
+ex) m["x"]++;
+
+< 원소 찾기 >
+map.find(key)  // 해당 위치의 iterator 반환. 없을 시 map.end() 반환.
+ex) if(m.find("x") != m.end()){}
+
+< 원소 삭제 >
+1. 특정 위치의 요소 삭제
+map.erase(iter);
+ex) m.erase(m.begin() + 2);
+
+2. key 값으로 삭제
+map.erase(key);
+ex) m.erase("x");
+
+< 순회 >
+ex)
+for(map<string, int>::iterator iter = m.begin();iter != m.end();iter++) {
 	iter->first  // key 값
 	iter->second // value 값
 }
-역순회 :
-for(map<string, int>::reverse_iterator iter = mp.rbegin();iter != mp.rend();iter++) {
+
+< 역순회 >
+for(map<string, int>::reverse_iterator iter = m.rbegin();iter != m.rend();iter++) {
 	iter->first  // key 값
 	iter->second // value 값
 }
