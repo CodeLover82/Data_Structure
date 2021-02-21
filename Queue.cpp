34dff14ee9887feb74dc5ff92abf8149bfbd509c
CodeLover82 <72126291+CodeLover82@users.noreply.github.com>
2021-02-21 21:10:59 +0900

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,6 +1,22 @@
+[ Queue ]
+
+FIFO(first-in first-out) 방식.
+
 < 선언 >
-선언 : queue<int> q;
-원소 추가 : q.push(3);
-제일 앞 원소 : q.front();
-제일 뒤 원소 : q.back();
-원소 삭제 : q.pop();
+ex) queue<int> q;
+
+< 원소 추가 >
+ex) q.push(3);
+
+< 제일 앞 원소 >
+q.front();
+
+< 제일 뒤 원소 >
+q.back();
+
+< 원소 삭제 >
+q.pop();
+
+< 큐의 공백 >
+q.empty();  // 비어있으면 true 아니면, false.
+ex) if(!q.empty()){}
