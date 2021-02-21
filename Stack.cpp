562815f07801bf86a2e42bdf79a4f8ca2c4170ae
CodeLover82 <72126291+CodeLover82@users.noreply.github.com>
2021-02-21 21:13:31 +0900

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,13 +1,20 @@
-/*
-
 #include <stack>
 
 [ Stack ]
 LIFO(last-in first-out) 방식.
 
-선언 : stack<int> st;
-원소 추가 : st.push(3);
-제일 위 원소 : st.top();
-원소 삭제 : st.pop();
+< 선언 >
+ex) stack<int> st;
 
-*/
+< 원소 추가 >
+ex) st.push(3);
+
+< 제일 위 원소 >
+st.top();
+
+< 원소 삭제 >
+st.pop();
+
+< 스택의 공백 >
+st.empty();  // 비어있으면 true 아니면, false.
+ex) if(!st.empty()){}
