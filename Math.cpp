c2fab154a885ff0fa4411f535a9dacdd0cd88515
CodeLover82 <72126291+CodeLover82@users.noreply.github.com>
2021-02-26 16:10:49 +0900

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -10,3 +10,8 @@ floor(1.6f); // 1
 
 < 반올림 >
 floor(d + 0.5);
+
+< 나머지 연산 >
+(a + b) % MOD = (a % MOD + b % MOD) % MOD
+(a - b) % MOD = (a % MOD - b % MOD + MOD) % MOD
+(a * b) % MOD = ((a % MOD) * (b % MOD)) % MOD
